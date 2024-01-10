e4cb1ca165bc53d2f0afea9ac8d9db1ce9a8d4ab
Mstfa773 <138675520+Mstfa773@users.noreply.github.com>
2024-01-10 18:32:01 +0300

    Update Mynoode.js

--- Mynoode.js
+++ Mynoode.js
@@ -37,7 +37,7 @@ class Dog extends Animal {
 class Main {
   public static void main(String[] args) {
     Animal myAnimal = new Animal();  // Create a Animal object
-    Animal myPig = new Pig();  // Create a Pig object
+    Animal myPig = new Pigss();  // Create a Pig object
     Animal myDog = new Dog();  // Create a Dog object
     myAnimal.animalSound();
     myPig.animalSound();
