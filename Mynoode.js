d7f350f65f4cdc1698f2b97f2cba4e0baa89cd83
Mstfa773 <138675520+Mstfa773@users.noreply.github.com>
2024-01-11 18:20:04 +0300

    Update Mynoode.js

--- Mynoode.js
+++ Mynoode.js
@@ -6,7 +6,7 @@ class Animal {
 
 class Pigs extends Animal {
   public void animalSound() {
-    System.out.println("The pisg says: wee wee ARE");
+    System.out.println("The pisg says: wee ARE");
   }
 }
 
